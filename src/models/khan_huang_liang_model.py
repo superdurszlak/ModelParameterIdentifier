9b1103b8af712966ab0f0b3ecff133cb09960bba
Szymon Durak <superdurszlak@gmail.com>
2019-05-27 21:20:16 +0200

    Implement analytical derivatives for MJC and KHL models

--- src/models/khan_huang_liang_model.py
+++ src/models/khan_huang_liang_model.py
@@ -35,3 +35,41 @@ class KhanHuangLiangModel(MaterialModel):
         hardening = ((1 - (math.log(strain_rate) / D_log)) ** n1) * (strain ** n0)
 
         return (A + B * hardening) * softening * rate_exp
+
+    def derivatives(self, strain: float, strain_rate: float, temperature: float):
+        parameters = self.params
+        labels = self.labels()
+
+        t_ref = 293.15
+        t_melt = 1425 + 273.15
+        t_h = (temperature - t_ref) / (t_melt - t_ref)
+        D0 = 1e6
+        D_log = math.log(D0)
+
+        A = parameters[0]
+        B = parameters[1]
+        n0 = parameters[2]
+        n1 = parameters[3]
+        C = parameters[4]
+        m = parameters[5]
+
+        s_safe = max(strain, 1e-9)
+        sr_safe = max(strain_rate, 1e-9)
+        th_safe = max(t_h, 1e-9)
+
+        rate_exp = strain_rate ** C
+        softening = (1 - t_h ** m)
+        ln_diff = (1 - (math.log(sr_safe) / D_log))
+        hardening = (ln_diff ** n1) * (strain ** n0)
+        full_strain_hardening = (A + B * hardening)
+
+        derivatives = {
+            labels[0]: rate_exp * softening,
+            labels[1]: rate_exp * softening * hardening,
+            labels[2]: B * hardening * math.log(s_safe) * softening * rate_exp,
+            labels[3]: B * (ln_diff ** n1) * math.log(ln_diff) * softening * rate_exp,
+            labels[4]: full_strain_hardening * softening * rate_exp * math.log(sr_safe),
+            labels[5]: - full_strain_hardening * rate_exp * (t_h ** m) * math.log(th_safe)
+        }
+
+        return derivatives
--- src/models/modified_johnson_cook_model.py
+++ src/models/modified_johnson_cook_model.py
@@ -36,3 +36,44 @@ class ModifiedJohnsonCookModel(MaterialModel):
         temp_dep = L1 + L2 * strain
 
         return (A1 * (strain ** n1)) * (1 + str_rate_dep * math.log(r_h)) * math.exp(temp_dep * t_h)
+
+    def derivatives(self, strain: float, strain_rate: float, temperature: float):
+        parameters = self.params
+        labels = self.labels()
+
+        r_ref = 1e-3
+        t_ref = 293.15
+        t_melt = 1425 + 273.15
+        t_h = (temperature - t_ref) / (t_melt - t_ref)
+        r_h = strain_rate / r_ref
+
+        A1 = parameters[0]
+        n1 = parameters[1]
+        b1 = parameters[2]
+        b2 = parameters[3]
+        b3 = parameters[4]
+        L1 = parameters[5]
+        L2 = parameters[6]
+
+        s_safe = max(strain, 1e-9)
+        sr_safe = max(r_h, 1e-9)
+
+        str_rate_dep = b1 + strain * (b2 + strain * b3)
+        temp_dep = L1 + L2 * strain
+        base_stress = (A1 * (strain ** n1))
+        rate_dependent = (1 + str_rate_dep * math.log(r_h))
+        thermal_dependent = math.exp(temp_dep * t_h)
+        base_rate_component_derivative = base_stress * math.log(sr_safe) * thermal_dependent
+        base_thermal_component_derivative = base_stress * rate_dependent * thermal_dependent * t_h
+
+        derivatives = {
+            labels[0]: (strain ** n1) * rate_dependent * thermal_dependent,
+            labels[1]: math.log(s_safe) * base_stress * rate_dependent * thermal_dependent,
+            labels[2]: base_rate_component_derivative,
+            labels[3]: base_rate_component_derivative * strain,
+            labels[4]: base_rate_component_derivative * (strain ** 2.0),
+            labels[5]: base_thermal_component_derivative,
+            labels[6]: base_thermal_component_derivative * strain
+        }
+
+        return derivatives
